f40d7e235e56078d80f987902953162b7b2da8db
Divineleo2001 <divineleo20@gmail.com>
2024-10-25 17:45:40 +0530

    chore: initialize Expo React Native application setup

---
+++ src/app/(app)/_layout.tsx
@@ -0,0 +1,64 @@
+/* eslint-disable react/no-unstable-nested-components */
+import { Redirect, SplashScreen, Tabs } from 'expo-router';
+import React, { useCallback, useEffect } from 'react';
+
+import { useAuth, useIsFirstTime } from '@/core';
+import {
+  Feed as FeedIcon,
+  Settings as SettingsIcon,
+} from '@/ui/icons';
+
+export default function TabLayout() {
+  const status = useAuth.use.status();
+  const [isFirstTime] = useIsFirstTime();
+  const hideSplash = useCallback(async () => {
+    await SplashScreen.hideAsync();
+  }, []);
+  useEffect(() => {
+    if (status !== 'idle') {
+      setTimeout(() => {
+        hideSplash();
+      }, 1000);
+    }
+  }, [hideSplash, status]);
+
+  if (isFirstTime) {
+    return <Redirect href="/onboarding" />;
+  }
+  if (status === 'signOut') {
+    return <Redirect href="/login" />;
+  }
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Products',
+          tabBarIcon: ({ color }) => <FeedIcon color={color} />,
+          
+          tabBarTestID: 'feed-tab',
+        }}
+      />
+
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          headerShown: false,
+          tabBarIcon: ({ color }) => <SettingsIcon color={color} />,
+          tabBarTestID: 'settings-tab',
+        }}
+      />
+    </Tabs>
+  );
+}
+
+// const CreateNewPostLink = () => {
+//   return (
+//     <Link href="/feed/add-post" asChild>
+//       <Pressable>
+//         <Text className="px-3 text-primary-300">Create</Text>
+//       </Pressable>
+//     </Link>
+//   );
+// };
---
+++ src/app/(app)/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { Text } from '@/ui';
+
+
+export default function Feed() {
+
+  return (
+    <SafeAreaView className="flex-1">
+  <Text>Hello</Text>
+    </SafeAreaView>
+  );
+}
